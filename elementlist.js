9e1e087f8e7fd9413636f5974e86b691023e2c05
Travis <travis@travis-ci.org>
2017-01-23 12:00:37 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","FNVi\\Mongo\\Action"],["c","FNVi\\Mongo\\BSON"],["c","FNVi\\Mongo\\Collection"],["c","FNVi\\Mongo\\Database"],["c","FNVi\\Mongo\\Document"],["c","FNVi\\Mongo\\Query"],["c","FNVi\\Mongo\\Schema"],["c","FNVi\\Mongo\\Status"],["c","FNVi\\Mongo\\SubDocument"],["c","FNVi\\Mongo\\Tools\\AggregationPipeline"],["c","FNVi\\Mongo\\Tools\\Query"],["c","FNVi\\Mongo\\Tools\\Update"]];
